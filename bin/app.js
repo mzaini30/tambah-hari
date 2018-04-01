8fdc7d417a495c15873fbbc94685135b609acf6e
mzaini30 <muhzaini30@gmail.com>
2018-04-01 09:39:35 +0000

    oke

--- bin/app.js
+++ bin/app.js
@@ -1,12 +1,14 @@
 jQuery(function(){
-	$('#hari').keyup(function(){
+	$('#hari, #harga').keyup(function(){
 		sekarang = new Date();
 		nanti = new Date();
 		tambah = Number($('#hari').val());
+		harga = Number($('#harga').val());
 		nanti.setDate(sekarang.getDate() + tambah);
 		// $('#hasil').val(nanti);
 		nanti_indo = nanti.toLocaleDateString('id-ID', { weekday: 'long', day: 'numeric', month: 'long', year: 'numeric' });
 		// $('#komentar').val('<!-- '+nanti_indo+' -->');
 		$('#jekyll').val(`'${nanti}' # ${nanti_indo}`);
+		$('#jumlah-harga').val(harga * tambah);
 	});
 });
